525d1620ba89afc1f775d5a28dd11d159af8dbab
JCHM-1 <j.c.huizinga.mora@st.hanze.nl>
2023-12-22 15:06:09 +0100

    Added Topbar AutoMaat

--- app/src/main/java/android/compose/MainScreen.kt
+++ app/src/main/java/android/compose/MainScreen.kt
@@ -1,33 +1,60 @@
 package android.compose
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Badge
 import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavDestination
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 
+@OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun MainScreen() {
     val navController = rememberNavController()
     Scaffold(
+        topBar = {
+                 CenterAlignedTopAppBar(
+                     colors = TopAppBarDefaults.topAppBarColors(),
+                     title = {
+                         Text(
+                             text = "AutoMaat",
+                             fontSize = MaterialTheme.typography.headlineMedium.fontSize,
+                             fontWeight = FontWeight.Bold
+                         )
+                     }
+                 )
+        },
         bottomBar = { BottomBar(navController = navController)}
-    ) { BottomNavGraph(navController = navController)
+    ) {
+        BottomNavGraph(navController = navController)
     }
+
 }
 
 @Composable
